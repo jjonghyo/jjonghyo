14a6460ac283f80e4d09cbc558bd4220ce5d2b70
eugene <jjonghyo@gmail.com>
2012-03-13 16:58:34 +0200

    3d commit

---
+++ depot/app/models/product.rb
@@ -0,0 +1,13 @@
+class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, :presence => true
+  validates :description, :length => { :maximum => 1000, 
+						  :message => 'koli4stvo simvolov ne dolzno privishat 1000'}
+  validates :description, :length => { :maximum => 100, :minimum =>1,
+						  :message => 'pole obiazatelno k zapolneniyu i koli4stvo simvolov ne dolzno privishat 100'}
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, :uniqueness => true
+  validates :image_url, :format => {
+  :with => %r{\.(gif|jpg|png)$}i,
+  :message => 'kartinka dolzna imet format  GIF, JPG ili PNG '
+}
+end
